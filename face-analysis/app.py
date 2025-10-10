b391dc15a1320769ad63c52c2efab4fb5261ffd9
Zax4r <zaharbedarik@gmail.com>
2025-10-11 01:00:12 +0300

    feat: cropper and red_eye_classification

--- face-analysis/app.py
+++ face-analysis/app.py
@@ -3,15 +3,13 @@ import numpy as np
 from deepface import DeepFace
 import mediapipe as mp
 from PIL import Image
-import torch
+from tensorflow.keras.preprocessing import image
 from torchvision import models, transforms
 from fastapi import FastAPI, UploadFile, File
 from typing import Dict
+import tensorflow as tf
 
 app = FastAPI()
-
-mp_face_mesh = mp.solutions.face_mesh
-face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True, max_num_faces=1, refine_landmarks=True)
 
 
 skin_transform = transforms.Compose([
@@ -24,10 +22,13 @@ skin_transform = transforms.Compose([
 skin_model = models.densenet121(weights=models.DenseNet121_Weights.DEFAULT)
 skin_model.eval()
 
-def detect_face_and_crop(image: np.ndarray, region: str = 'face') -> np.ndarray:
+eye_model = tf.keras.models.load_model('red_eye_model.keras')
 
-    if image is None:
-        raise ValueError("Изображение не загружено")
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True, max_num_faces=1, refine_landmarks=True)
+padding = 5
+
+def detect_face_and_crop(image: np.ndarray, region: str = 'face') -> np.ndarray:
     
     rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = face_mesh.process(rgb_image)
@@ -40,19 +41,28 @@ def detect_face_and_crop(image: np.ndarray, region: str = 'face') -> np.ndarray:
         y_min = min([lm.y for lm in landmarks.landmark]) * h
         x_max = max([lm.x for lm in landmarks.landmark]) * w
         y_max = max([lm.y for lm in landmarks.landmark]) * h
-        crop = image[int(y_min):int(y_max), int(x_min):int(x_max)]
     
-    elif region == 'eyes':
-        left_eye = landmarks.landmark[33]  
-        right_eye = landmarks.landmark[263]
-        x_min = min(left_eye.x, right_eye.x) * w - 50
-        y_min = min(left_eye.y, right_eye.y) * h - 50
-        x_max = max(left_eye.x, right_eye.x) * w + 50
-        y_max = max(left_eye.y, right_eye.y) * h + 50
-        crop = image[int(y_min):int(y_max), int(x_min):int(x_max)]
+    elif region == 'left_eye':
+        left_eye_indices = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246]
+        left_eye_points = [landmarks.landmark[i] for i in left_eye_indices]
+        
+        x_min = min([lm.x for lm in left_eye_points]) * w - padding  
+        y_min = min([lm.y for lm in left_eye_points]) * h - padding
+        x_max = max([lm.x for lm in left_eye_points]) * w + padding
+        y_max = max([lm.y for lm in left_eye_points]) * h + padding
 
+    elif region == 'right_eye':
+        right_eye_indices = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398]
+        right_eye_points = [landmarks.landmark[i] for i in right_eye_indices]
+        
+        x_min = min([lm.x for lm in right_eye_points]) * w - padding
+        y_min = min([lm.y for lm in right_eye_points]) * h - padding
+        x_max = max([lm.x for lm in right_eye_points]) * w + padding
+        y_max = max([lm.y for lm in right_eye_points]) * h + padding
     
+    crop = image[int(y_min):int(y_max), int(x_min):int(x_max)]
     return crop
+
 
 def analyze_emotions(image: np.ndarray) -> Dict:
 
@@ -63,6 +73,7 @@ def analyze_emotions(image: np.ndarray) -> Dict:
       )
 
       emotions = result[0]['emotion']
+      print(emotions.keys())
       stress = emotions.get('sad', 0) + emotions.get('fear', 0)
       fatigue = emotions.get('neutral', 0)
       anxiety = emotions.get('fear', 0)
@@ -74,31 +85,33 @@ def analyze_emotions(image: np.ndarray) -> Dict:
       }
 
 
+def red_eye_classification(eye_crop_image):
+
+    img_resized = cv2.resize(eye_crop_image, (224, 224))
+    img_array = image.img_to_array(img_resized)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.0
+
+
+    prediction = eye_model.predict(img_array, verbose=0)
+    predicted_class_idx = np.argmax(prediction, axis=1)[0]
+    confidence = float(np.max(prediction))
+
+
+    return 'red' if predicted_class_idx else 'healthy', confidence
+
 def analyze_eyes(image: np.ndarray) -> Dict:
-    try:
-        eye_crop = detect_face_and_crop(image, 'eyes')
-        hsv = cv2.cvtColor(eye_crop, cv2.COLOR_BGR2HSV)
-        
-        red_lower = np.array([0, 70, 50])
-        red_upper = np.array([10, 255, 255])
-        red_mask = cv2.inRange(hsv, red_lower, red_upper)
-        redness = np.sum(red_mask) / (eye_crop.shape[0] * eye_crop.shape[1])
-        
-        gray = cv2.cvtColor(eye_crop, cv2.COLOR_BGR2GRAY)
-        dark_circles = 1 - (np.mean(gray) / 255) 
-        
-        yellow_lower = np.array([20, 100, 100])
-        yellow_upper = np.array([30, 255, 255])
-        yellow_mask = cv2.inRange(hsv, yellow_lower, yellow_upper)
-        yellowness = np.sum(yellow_mask) / (eye_crop.shape[0] * eye_crop.shape[1])
-        
-        return {
-            'redness': redness,
-            'dark_circles': dark_circles,
-            'yellowness': yellowness
-        }
-    except Exception as e:
-        return {'error': str(e)}
+    cropped = detect_face_and_crop(image,'left_eye')
+    le_class,le_conf = red_eye_classification(cropped)
+    cropped = detect_face_and_crop(image,'right_eye')
+    re_class,re_conf = red_eye_classification(cropped)
+    return {
+        'left_eye_class': le_class,
+        'left_eye_confidence': le_conf,
+        'rigth_eye_class': re_class,
+        'right_eye_confidence':re_conf
+    }
+
 
 def analyze_photo(image: np.ndarray) -> Dict:
     results = {
